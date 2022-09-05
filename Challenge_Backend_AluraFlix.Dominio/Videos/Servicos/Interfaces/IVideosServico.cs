53bbfa07c98db13167de2a69b5fe36fb87914fec
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-05 20:58:23 -0300

    Adiciona Interface de Servico de videos

---
+++ Challenge_Backend_AluraFlix.Dominio/Videos/Servicos/Interfaces/IVideosServico.cs
@@ -0,0 +1,19 @@
+﻿using Challenge_Backend_AluraFlix.Dominio.Videos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Videos.Servicos.Interfaces
+{
+    public interface IVideosServico
+    {
+        Video Validar(int id);
+        Video Instanciar(string titulo, string desc, string url);
+        Video Inserir(Video video);
+        Video Editar(Video video);
+        void Deletar(Video video);
+        IQueryable<Video> Query();
+    }
+}
