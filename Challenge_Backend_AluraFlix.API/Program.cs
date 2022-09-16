07ddae292b0610384beb059e13c45481226f002a
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-16 01:19:35 -0300

    Adiciona configuração de ISession do Fluent NHibernate

--- Challenge_Backend_AluraFlix.API/Program.cs
+++ Challenge_Backend_AluraFlix.API/Program.cs
@@ -1,3 +1,8 @@
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using ISession = NHibernate.ISession;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISessionFactory>(factory =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySql");
+    return Fluently.Configure()
+                        .Database(MySQLConfiguration.Standard
+                            .ConnectionString(connectionString)
+                            .ShowSql())
+                        .BuildSessionFactory();
+});
+
+builder.Services.AddSingleton<ISession>(factory => factory.GetService<ISessionFactory>()?.OpenSession());
 
 var app = builder.Build();
 
