645c73f3865b8f334775c6781935d39c352fb66f
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-17 16:13:30 -0300

    Adiciona Repositorio generico

---
+++ Challenge_Backend_AluraFlix.Dominio/Genericos/Repositorios/IGenericosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Genericos.Repositorios
+{
+    public interface IGenericosRepositorio<T> where T : class
+    {
+        T Recuperar(int id);
+        T Inserir(T inserir);
+        T Editar(T editar);
+        void Deletar(T deletar);
+        IQueryable<T> Query();
+    }
+}
