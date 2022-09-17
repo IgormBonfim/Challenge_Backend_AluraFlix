4a21d2d072d736de6afdaa6dcb5122fd15dc8ff8
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-17 01:12:58 -0300

    Adiciona validações a entidade Video

--- Challenge_Backend_AluraFlix.Dominio/Videos/Entidades/Video.cs
+++ Challenge_Backend_AluraFlix.Dominio/Videos/Entidades/Video.cs
@@ -35,27 +35,27 @@ namespace Challenge_Backend_AluraFlix.Dominio.Videos.Entidades
         public void SetTituloVideo(string? titulo)
         {
             if (string.IsNullOrWhiteSpace(titulo))
-            {
                 throw new Exception("Titulo não pode ser vazio!");
-            }
+            if (titulo.Length > 100)
+                throw new Exception("O Titulo não deve conter o número de caracteres superior a 100!");
             TituloVideo = titulo;
         }
 
         public void SetDescVideo(string? desc)
         {
             if (desc != null && desc.Length < 5)
-            {
                 throw new Exception("Descrição não deve ser inferior à 5 caracteres!");
-            }
+            if (desc.Length > 255)
+                throw new Exception("A Descrição não deve conter o número de caracteres superior a 255!");
             DescVideo = desc;
         }
 
         public void SetUrlVideo(string? url)
         {
             if (string.IsNullOrWhiteSpace(url))
-            {
                 throw new Exception("URL não pode ser vazio!");
-            }
+            if (url.Length > 255)
+                throw new Exception("A URL não deve conter o número de caracteres superior a 255!");
             UrlVideo = url;
         }
     }
