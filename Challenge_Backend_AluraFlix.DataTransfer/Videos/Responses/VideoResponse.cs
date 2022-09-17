1676641d29f0fb38bd2d3b635d055e9f0073831a
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-17 01:13:14 -0300

    Adiciona VideoResponse

---
+++ Challenge_Backend_AluraFlix.DataTransfer/Videos/Responses/VideoResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.DataTransfer.Videos.Responses
+{
+    public class VideoResponse
+    {
+        public int VideoId { get; set; }
+        public string TituloVideo { get; set; }
+        public string? DescVideo { get; set; }
+        public string UrlVideo { get; set; }
+    }
+}
