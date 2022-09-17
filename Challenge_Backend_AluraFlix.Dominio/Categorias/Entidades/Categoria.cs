51d60f5decd6f2ac0a2e32e80a7947d58cd407e5
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-17 15:23:56 -0300

    Adiciona Entidade de categoria

---
+++ Challenge_Backend_AluraFlix.Dominio/Categorias/Entidades/Categoria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Categorias.Entidades
+{
+    public class Categoria
+    {
+        public virtual int IdCategoria { get; protected set; }
+        public virtual string TituloCategoria { get; protected set; }
+        public virtual string CorCategoria { get; protected set; }
+
+        public Categoria()
+        {
+              
+        }
+
+        public Categoria(string? titulo, string cor)
+        {
+            SetTituloCategoria(titulo);
+            SetCorCategoria(cor);
+        }
+        
+        public virtual void SetIdCategoria(int id)
+        {
+            IdCategoria = id;
+        }
+
+        public virtual void SetTituloCategoria(string? titulo)
+        {
+            if (string.IsNullOrWhiteSpace(titulo))
+                throw new Exception("O campo título é o obrigatório!");
+            TituloCategoria = titulo;
+        }
+
+        public virtual void SetCorCategoria(string? cor)
+        {
+            if (string.IsNullOrWhiteSpace(cor))
+                throw new Exception("O campo cor é obrigatório!");
+            CorCategoria = cor;
+        }
+    }
+}
