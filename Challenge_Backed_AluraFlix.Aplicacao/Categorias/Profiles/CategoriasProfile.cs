85e3a191697882283adae95424709a1b6be619e7
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-17 23:50:51 -0300

    Adiciona CategoriaInserirRequest e mapeamento com automapper

---
+++ Challenge_Backed_AluraFlix.Aplicacao/Categorias/Profiles/CategoriasProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Challenge_Backend_AluraFlix.DataTransfer.Categorias.Requests;
+using Challenge_Backend_AluraFlix.Dominio.Categorias.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backed_AluraFlix.Aplicacao.Categorias.Profiles
+{
+    public class CategoriasProfile : Profile
+    {
+        public CategoriasProfile()
+        {
+            CreateMap<CategoriaInserirRequest, Categoria>();
+        }
+    }
+}
