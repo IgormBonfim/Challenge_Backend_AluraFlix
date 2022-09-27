12e291f1dc2d72ad75ef8764004b4daa040fdec4
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 10:30:13 -0300

    Adiciona mapeamento de usuarios

---
+++ Challenge_Backend_AluraFlix.Infra/Usuarios/Mapeamentos/UsuariosPasswordMap.cs
@@ -0,0 +1,24 @@
+﻿using Challenge_Backend_AluraFlix.Dominio.Usuarios.Entidades;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Infra.Usuarios.Mapeamentos
+{
+    public class UsuariosPasswordMap : ClassMap<UsuarioPassword>
+    {
+        public UsuariosPasswordMap()
+        {
+            Schema("CHALLENGE_BACKEND_ALURAFLIX");
+            Table("USUARIOS");
+            Id(x => x.IdUsuario).Column("IdUsuario").GeneratedBy.Assigned();
+            Map(x => x.NomeUsuario).Column("NomeUsuario");
+            Map(x => x.SobrenomeUsuario).Column("SobrenomeUsuario");
+            Map(x => x.EmailUsuario).Column("EmailUsuario");
+            Map(x => x.SenhaUsuario).Column("SenhaUsuario");
+        }
+    }
+}
