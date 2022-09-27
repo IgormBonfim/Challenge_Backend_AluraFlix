cc313f7eb1fefff22af4e3fac9a24a5cebcf19db
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 10:31:10 -0300

    Remove Guid de UsuarioRegistrarRequest

--- Challenge_Backend_AluraFlix.DataTransfer/Usuarios/Requests/UsuarioRegistrarRequest.cs
+++ Challenge_Backend_AluraFlix.DataTransfer/Usuarios/Requests/UsuarioRegistrarRequest.cs
@@ -8,7 +8,6 @@ namespace Challenge_Backend_AluraFlix.DataTransfer.Usuarios.Requests
 {
     public class UsuarioRegistrarRequest
     {
-        public Guid? IdUsuario { get; set; }
         public string NomeUsuario { get; set; }
         public string SobrenomeUsuario { get; set; }
         public string EmailUsuario { get; set; }
