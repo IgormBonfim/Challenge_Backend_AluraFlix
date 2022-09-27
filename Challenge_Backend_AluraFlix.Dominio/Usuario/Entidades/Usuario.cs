dcd049b092e3405581b520cacc698e317de5e3fa
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 08:42:59 -0300

    Adiciona entidades de dominio de Usuario

---
+++ Challenge_Backend_AluraFlix.Dominio/Usuario/Entidades/Usuario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Usuario.Entidades
+{
+    public class Usuario
+    {
+        public virtual Guid IdUsuario { get; protected set; }
+        public virtual string NomeUsuario { get; protected set; }
+        public virtual string SobrenomeUsuario { get; protected set; }
+        public virtual string EmailUsuario { get; protected set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(string? nomeUsuario, string? sobrenomeUsuario, string? emailUsuario)
+        {
+            SetNomeUsuario(nomeUsuario);
+            SetSobrenomeUsuario(sobrenomeUsuario);
+            SetEmailUsuario(emailUsuario);
+
+        }
+
+        public virtual void SetIdUsuario(Guid idUsuario)
+        {
+            IdUsuario = idUsuario;
+        }
+
+        public virtual void SetNomeUsuario(string? nomeUsuario)
+        {
+            if (string.IsNullOrWhiteSpace(nomeUsuario))
+                throw new Exception("O campo Nome é obrigatório!");
+            NomeUsuario = nomeUsuario;
+        }
+
+        public virtual void SetSobrenomeUsuario(string? sobrenome)
+        {
+            if (string.IsNullOrWhiteSpace(sobrenome))
+                throw new Exception("O campo Sobrenome é obrigatório!");
+            SobrenomeUsuario= sobrenome;
+        }
+
+        public virtual void SetEmailUsuario(string? email)
+        {
+            Regex rg = new Regex(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new Exception("O campo Email é obrigatório!");
+
+            if (!rg.IsMatch(email))
+                throw new Exception("Formato de email inválido!");
+            EmailUsuario = email;
+        }
+
+    }
+}
---
+++ Challenge_Backend_AluraFlix.Dominio/Usuario/Entidades/UsuarioPassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Usuario.Entidades
+{
+    public class UsuarioPassword : Usuario
+    {
+        public virtual string SenhaUsuario { get; protected set; }
+
+        public UsuarioPassword()
+        {
+
+        }
+
+        public UsuarioPassword(string? nomeUsuario, string? sobrenomeUsuario, string? emailUsuario, string? senha) : base(nomeUsuario, sobrenomeUsuario, emailUsuario)
+        {
+            SetSenhaUsuario(senha);
+        }
+
+        public virtual void SetSenhaUsuario(string? senha)
+        {
+            if (string.IsNullOrWhiteSpace(senha))
+                throw new Exception("O campo Senha é obrigatório!");
+            SenhaUsuario = senha;
+        }
+    }
+}
