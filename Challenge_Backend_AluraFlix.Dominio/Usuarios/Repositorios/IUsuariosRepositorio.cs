d80a8ffafcf9494cd61153470ae37bf46bab7b2c
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 10:29:01 -0300

    Adiciona Repositorio de Usuario

---
+++ Challenge_Backend_AluraFlix.Dominio/Usuarios/Repositorios/IUsuariosRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Challenge_Backend_AluraFlix.Dominio.Usuarios.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Dominio.Usuarios.Repositorios
+{
+    public interface IUsuariosRepositorio
+    {
+        void Inserir(UsuarioPassword usuario);
+        IQueryable<UsuarioPassword> Buscar();
+    }
+}
---
+++ Challenge_Backend_AluraFlix.Infra/Usuarios/UsuariosRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using Challenge_Backend_AluraFlix.Dominio.Usuarios.Entidades;
+using Challenge_Backend_AluraFlix.Dominio.Usuarios.Repositorios;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Infra.Usuarios
+{
+    public class UsuariosRepositorio : IUsuariosRepositorio
+    {
+        private readonly ISession session;
+
+        public UsuariosRepositorio(ISession session)
+        {
+            this.session = session;
+        }
+
+        public IQueryable<UsuarioPassword> Buscar()
+        {
+            return session.Query<UsuarioPassword>();
+        }
+
+        public void Inserir(UsuarioPassword usuario)
+        {
+            session.Save(usuario);
+        }
+    }
+}
