f5d087e2be59a96cbcfac3a65c9766749e79b3a7
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 10:30:52 -0300

    Adiciona metodo de login no servico de aplicação

--- Challenge_Backed_AluraFlix.Aplicacao/Usuarios/Servicos/AutenticacaoAppServico.cs
+++ Challenge_Backed_AluraFlix.Aplicacao/Usuarios/Servicos/AutenticacaoAppServico.cs
@@ -1,7 +1,9 @@
 ﻿using Challenge_Backend_AluraFlix.Aplicacao.Usuarios.Servicos.Interfaces;
 using Challenge_Backend_AluraFlix.DataTransfer.Usuarios.Requests;
 using Challenge_Backend_AluraFlix.DataTransfer.Usuarios.Responses;
-using Challenge_Backend_AluraFlix.Dominio.Usuario.Servicos.Interfaces;
+using Challenge_Backend_AluraFlix.Dominio.Usuarios.Repositorios;
+using Challenge_Backend_AluraFlix.Dominio.Usuarios.Servicos.Interfaces;
+using NHibernate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,32 +15,79 @@ namespace Challenge_Backend_AluraFlix.Aplicacao.Usuarios.Servicos
     public class AutenticacaoAppServico : IAutenticacaoAppServico
     {
         private readonly IGeradorTokenJwt tokenJwt;
+        private readonly ISession session;
+        private readonly IUsuariosServico usuariosServico;
+        private readonly IUsuariosRepositorio usuariosRepositorio;
 
-        public AutenticacaoAppServico(IGeradorTokenJwt tokenJwt)
+        public AutenticacaoAppServico(IGeradorTokenJwt tokenJwt, ISession session, IUsuariosServico usuariosServico, IUsuariosRepositorio usuariosRepositorio)
         {
             this.tokenJwt = tokenJwt;
+            this.session = session;
+            this.usuariosServico = usuariosServico;
+            this.usuariosRepositorio = usuariosRepositorio;
         }
         public AutenticacaoResponse Login(UsuarioLoginRequest usuarioLoginRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var query = usuariosRepositorio.Buscar()
+                    .Where(x => x.EmailUsuario == usuarioLoginRequest.EmailUsuario)
+                    .Where(x => x.SenhaUsuario == usuarioLoginRequest.SenhaUsuario);
+
+                var usuario = usuariosServico.Validar(query);
+
+                string tokenGerado = tokenJwt.GerarToken(usuario.IdUsuario, usuario.NomeUsuario, usuario.SobrenomeUsuario);
+
+                var response = new AutenticacaoResponse
+                {
+                    IdUsuario = usuario.IdUsuario,
+                    NomeUsuario = usuario.NomeUsuario,
+                    SobrenomeUsuario = usuario.SobrenomeUsuario,
+                    EmailUsuario = usuario.EmailUsuario,
+                    Token = tokenGerado
+                };
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         public AutenticacaoResponse Registrar(UsuarioRegistrarRequest usuarioRegistrarRequest)
         {
-            usuarioRegistrarRequest.IdUsuario = Guid.NewGuid();
+            ITransaction transacao = session.BeginTransaction();
 
-            string tokenGerado = tokenJwt.GerarToken(usuarioRegistrarRequest.IdUsuario.Value, usuarioRegistrarRequest.NomeUsuario, usuarioRegistrarRequest.SobrenomeUsuario);
+            try
+            {
+                var novoUsuario = usuariosServico.Instanciar(usuarioRegistrarRequest.NomeUsuario, usuarioRegistrarRequest.SobrenomeUsuario, usuarioRegistrarRequest.EmailUsuario, usuarioRegistrarRequest.SenhaUsuario);
+                var usuario = usuariosServico.Inserir(novoUsuario);
 
-            var response = new AutenticacaoResponse
-            { 
-                IdUsuario = usuarioRegistrarRequest.IdUsuario.Value,
-                NomeUsuario = usuarioRegistrarRequest.NomeUsuario,
-                SobrenomeUsuario = usuarioRegistrarRequest.SobrenomeUsuario,
-                EmailUsuario = usuarioRegistrarRequest.EmailUsuario,
-                Token = tokenGerado
-            };
+                string tokenGerado = tokenJwt.GerarToken(usuario.IdUsuario, usuario.NomeUsuario, usuario.SobrenomeUsuario);
 
-            return response;
+                if (transacao.IsActive)
+                    transacao.Commit();
+
+                var response = new AutenticacaoResponse
+                {
+                    IdUsuario = usuario.IdUsuario,
+                    NomeUsuario = usuario.NomeUsuario,
+                    SobrenomeUsuario = usuario.SobrenomeUsuario,
+                    EmailUsuario = usuario.EmailUsuario,
+                    Token = tokenGerado
+                };
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                if (transacao.IsActive)
+                    transacao.Rollback();
+                throw e;
+            }
+
+            
         }
     }
 }
