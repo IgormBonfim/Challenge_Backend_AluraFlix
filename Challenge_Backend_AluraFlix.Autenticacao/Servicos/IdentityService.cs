033629a77b0276cfbfaf2fca2f2d83d1ac4610c9
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-29 11:05:02 -0300

    Adiciona implementação de IdentityServico

---
+++ Challenge_Backend_AluraFlix.Autenticacao/Servicos/IdentityService.cs
@@ -0,0 +1,69 @@
+﻿using Challenge_Backend_AluraFlix.Aplicacao.Usuarios.Servicos.Interfaces;
+using Challenge_Backend_AluraFlix.DataTransfer.Usuarios.Requests;
+using Challenge_Backend_AluraFlix.DataTransfer.Usuarios.Responses;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Backend_AluraFlix.Autenticacao.Servicos
+{
+    public class IdentityService : IIdentityServico
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        //private readonly IOptions<JwtOptions> jwtOptions;
+
+        public IdentityService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager/*, IOptions<JwtOptions> jwtOptions*/)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            //this.jwtOptions = jwtOptions;
+        }
+
+        public async Task<UsuarioCadastroResponse> CadastrarUsuario(UsuarioCadastroRequest usuarioCadastro)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = usuarioCadastro.Email,
+                Email = usuarioCadastro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(identityUser, usuarioCadastro.Senha);
+            if (result.Succeeded)
+                await userManager.SetLockoutEnabledAsync(identityUser, false);
+
+            var usuarioCadastroResponse = new UsuarioCadastroResponse(result.Succeeded);
+            if (!result.Succeeded && result.Errors.Count() > 0)
+                usuarioCadastroResponse.AdicionarErros(result.Errors.Select(r => r.Description));
+
+            return usuarioCadastroResponse;
+        }
+
+        public async Task<UsuarioLoginResponse> Login(UsuarioLoginRequest usuarioLoginRequest)
+        {
+            var result = await signInManager.PasswordSignInAsync(usuarioLoginRequest.Email, usuarioLoginRequest.Senha, false, true);
+            //if (result.Succeeded)
+            //    return await GerarToken(usuarioLoginRequest.Email);
+
+            var usuarioLoginResponse = new UsuarioLoginResponse(result.Succeeded);
+            if (!result.Succeeded)
+            {
+                if (result.IsLockedOut)
+                    usuarioLoginResponse.AdicionarErro("Essa conta está bloqueada");
+                else if (result.IsNotAllowed)
+                    usuarioLoginResponse.AdicionarErro("Essa conta não tem permissão para fazer login");
+                else if (result.RequiresTwoFactor)
+                    usuarioLoginResponse.AdicionarErro("É necessário confirmar o login no seu segundo fator de autenticação");
+                else
+                    usuarioLoginResponse.AdicionarErro("Usuário ou senha estão incorretos");
+            }
+
+            return usuarioLoginResponse;
+        }
+    }
+}
